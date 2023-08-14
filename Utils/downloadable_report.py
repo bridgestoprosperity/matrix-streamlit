2ed8398368d84201fe77934824dbbb76389e4f56
AndreMwendwa <mwendwa.kiko@i-carre.net>
2023-08-14 13:03:03 -0400

    Changes to report layout 2

--- Utils/downloadable_report.py
+++ Utils/downloadable_report.py
@@ -3,9 +3,9 @@ import datapane as dp
 
 def report_builder(span, total_costs):
     report = dp.Select(
-        dp.Group(
+
             dp.Text(f"Total span: {span} m"),
             dp.Table(total_costs)
-        )
+
     )
     dp.save_report(report, path='report.html', open=True)
